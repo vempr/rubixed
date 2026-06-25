394de2b8212f5ae28b563ad153120fb4b406374e
vempr <alexnguyena6@gmail.com>
2026-06-25 23:36:57 +0200

    build (2): fix windows build

--- main.c
+++ main.c
@@ -13,40 +13,14 @@
 
 #if defined(_WIN32)
 	#include <direct.h>
-	#include <windows.h>
 	#define mkdir_cache() _mkdir("cache")
 #else
-	#include <unistd.h>
-	#include <libgen.h>
 	#include <sys/stat.h>
 	#define mkdir_cache() mkdir("cache", 0777)
 #endif
 
-void set_working_directory_to_exe(int argc, char* argv[]) {
-	#if defined(_WIN32)
-		char exe_path[MAX_PATH];
-		if (GetModuleFileNameA(NULL, exe_path, MAX_PATH) != 0) {
-			char* last_slash = strrchr(exe_path, '\\');
-			if (last_slash != NULL) {
-				*last_slash = '\0';
-				_chdir(exe_path);
-			}
-		}
-	#else
-		if (argc > 0 && argv[0] != NULL) {
-			char* dir = strdup(argv[0]);
-			if (dir) {
-				char* dir_path = dirname(dir);
-				int chdir_res = chdir(dir_path);
-				(void)chdir_res;
-				free(dir);
-			}
-		}
-	#endif
-}
-
 int main(int argc, char* argv[]) {
-	set_working_directory_to_exe(argc, argv);
+	ChangeDirectory(GetApplicationDirectory());
 	mkdir_cache();
 	
 	App app = {0};
